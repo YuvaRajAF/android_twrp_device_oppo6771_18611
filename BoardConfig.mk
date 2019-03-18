e9bcd5be0dd53891456ec18d321b870988dabb8e
Ansh Singh <anshsingh.14.lko@gmail.com>
2019-03-18 10:38:43 +0530

    Create BoardConfig.mk
    Initial Commit will update later

---
+++ BoardConfig.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2019 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/oppo/cph1859
+
+TARGET_BOARD_PLATFORM := mt6771
+TARGET_BOOTLOADER_BOARD_NAME := cph1859
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 
+BOARD_FLASH_BLOCK_SIZE := 
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_SUPPRESS_SECURE_ERASE := 
+BOARD_HAS_NO_MISC_PARTITION := 
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := 
+BOARD_SUPPRESS_EMMC_WIPE := 
+TW_INPUT_BLACKLIST := 
+include $(LOCAL_PATH)/kernel.mk
+include device/generic/twrpbuilder/BoardConfig64.mk
