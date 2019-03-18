244ca924df10876b7bb906186a360cb923f23bec
Ansh Singh <anshsingh.14.lko@gmail.com>
2019-03-18 11:09:34 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+# Copyright (C) 2019 The TwrpBuilder Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 
 ifneq ($(filter cph1859,$(TARGET_DEVICE)),)
 
-LOCAL_PATH := device/oppo/cph1859/
+LOCAL_PATH := device/oppo/cph1861/
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
