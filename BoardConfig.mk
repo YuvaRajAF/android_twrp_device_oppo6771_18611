8cbd715ff5d28bd6046623ffc58215e1e76b0966
Best TipsYT <yuvarajv952@gmail.com>
2019-05-07 09:29:33 +0530

    Add files via upload

---
+++ BoardConfig.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/realme/oppo6771_18611
+
+TARGET_BOARD_PLATFORM := mt6771
+TARGET_BOOTLOADER_BOARD_NAME := oppo6771_18611
+
+# ARCHITECTURE
+TARGET_ARCH := arm64
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := armeabi-v7a
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := generic
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_FLASH_BLOCK_SIZE := 0
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+include $(LOCAL_PATH)/kernel.mk
+include device/generic/twrpbuilder/BoardConfig64.mk
+
+#kernel
+BOARD_KERNEL_IMAGE_NAME := kernel
+TARGET_PREBUILT_KERNEL := device/realme/oppo6771_18611/kernel
+TARGET_KERNEL_SOURCE := kernel/realme
+TARGET_KERNEL_CONFIG := oppo6771_18611_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
